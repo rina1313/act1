281d57865f27fd056e1162f5fe3ad8d3b43a0202
kate <marinaragobrio@gmail.com>
2022-04-11 19:33:00 +0800

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './pages/about/about.component';
+import { CredentialsComponent } from './pages/credentials/credentials.component';
+
+const routes: Routes = [
+  {path: "about", component: AboutComponent},
+  {path: "credentials", component: CredentialsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
