934f015207502d97c7d8a0698328a3e1cd4e8bea
kate <marinaragobrio@gmail.com>
2022-04-11 18:36:42 +0800

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CredentialsComponent } from './pages/credentials/credentials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CredentialsComponent
   ],
   imports: [
     BrowserModule,
